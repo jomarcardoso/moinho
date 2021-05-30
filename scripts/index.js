d301086c3b520e5550474bb97852b5a13b63b372
Jomar <jomar.cardoso@cwi.com.br>
2021-05-30 01:22:06 -0300

    feat: 🎸 players

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,51 @@
-const { fromEvent } = rxjs;
+const { fromEvent, Observable, Subject } = rxjs;
 const { scan } = rxjs.operators;
 const buttons = document.querySelectorAll('button');
 
-function listen(button) {
-  fromEvent(button, 'click')
-    .pipe(scan(() => true, false))
-    .subscribe(piece => piece && button.classList.add('checked'));
+const players = {
+  ONE: 'p1',
+  TWO: 'p2',
+  NONE: 'NONE',
+};
+
+function generateBoardDots(quantity) {
+  const array = [];
+
+  for(i = 0; i < quantity; i++) {
+    array.push({
+      player: players.NONE
+    })
+  }
+
+  return array;
 }
 
-buttons.forEach(listen);
+const boardDots = generateBoardDots(24);
+let currentPlayer = players.ONE;
+
+const subject = new Subject();
+const observable = fromEvent(buttons, 'click')
+
+function createDotsSubscribers() {
+  buttons.forEach((button) => {
+    subject.subscribe({
+      next: (e) => {
+        const currentPosition = e.target.dataset.position;
+        if (currentPosition === button.dataset.position) {
+          button.classList.add(`checked-${currentPlayer}`)
+          boardDots[currentPosition] = { player: currentPlayer };
+
+          if (currentPlayer === players.ONE) {
+            currentPlayer = players.TWO;
+          } else {
+            currentPlayer = players.ONE;
+          }
+        }
+      }
+    })
+  })
+}
+
+createDotsSubscribers();
+
+observable.subscribe(subject);

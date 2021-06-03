067c6f844847affc14d5b5b8ca8ada86fc6d75d1
Jomar <jomar.cardoso@cwi.com.br>
2021-06-03 01:21:12 -0300

    feat: 🎸 scan state

--- scripts/index.js
+++ scripts/index.js
@@ -8,26 +8,107 @@ const players = {
   NONE: 'NONE',
 };
 
+const GAME = {
+  positions: [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ],
+  nextPlayer: players.ONE
+}
+
 const subject = new Subject();
 
 const dot0Observable = fromEvent(buttons[0], 'click').subscribe(subject);
 const dot1Observable = fromEvent(buttons[1], 'click').subscribe(subject);
 const dot2Observable = fromEvent(buttons[2], 'click').subscribe(subject);
 
-const currentPlayer = subject.subscribe({
-  next: (e) => console.log(e),
+const game$ = subject.pipe(scan((state = [], event) => {
+  const lastState = state[state.length - 1];
+  const index = event.target.dataset.position;
+  const positions = [...lastState.positions];
+  const { nextPlayer } = lastState;
+
+  positions[index] = nextPlayer;
+
+  const newState = {
+    positions,
+    nextPlayer: nextPlayer === players.ONE ? players.TWO : players.ONE,
+  };
+
+  return [...state, newState];
+}, [GAME]));
+
+const currentPlayer = game$.subscribe({
+  // next: (e) => console.log('j', e),
 })
 
-const dot0Subscriber = subject.subscribe({
-  next: (e) => console.log(e)
+const dot0Subscriber = game$.subscribe({
+  next: (game = []) => {
+    const state = game[game.length - 1];
+    const button = document.querySelector('[data-position="0"]');
+
+    if (state.positions[0] === players.ONE) {
+      button.classList.add(`checked-${players.ONE}`)
+    }
+
+    if (state.positions[0] === players.TWO) {
+      button.classList.add(`checked-${players.TWO}`)
+    }
+  }
 });
 
-const dot1Subscriber = subject.subscribe({
-  next: (e) => console.log(e)
+const dot1Subscriber = game$.subscribe({
+  next: (game = []) => {
+    const state = game[game.length - 1];
+    const button = document.querySelector('[data-position="1"]');
+
+    if (state.positions[1] === players.ONE) {
+      button.classList.add(`checked-${players.ONE}`)
+    }
+
+    if (state.positions[1] === players.TWO) {
+      button.classList.add(`checked-${players.TWO}`)
+    }
+  }
 });
 
-const dot2Subscriber = subject.subscribe({
-  next: (e) => console.log(e)
+const dot2Subscriber = game$.subscribe({
+  next: (game = []) => {
+    const state = game[game.length - 1];
+    const button = document.querySelector('[data-position="2"]');
+
+    console.log(state.positions[2], players.ONE)
+
+    if (state.positions[2] === players.ONE) {
+      button.classList.add(`checked-${players.ONE}`)
+    }
+
+    if (state.positions[2] === players.TWO) {
+      button.classList.add(`checked-${players.TWO}`)
+    }
+  }
 });
 
 // function generateBoardDots(quantity) {
